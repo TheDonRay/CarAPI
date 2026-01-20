cde0998074ffd7105854197e7d60eede2e0c6f09
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-19 19:27:17 -0500

    working

---
+++ backend/routes/userPreference.route.js
@@ -0,0 +1,7 @@
+const express = require('express'); 
+const userPref = express.Router(); 
+const userPrefController = require('../controllers/')
+
+userPref.post('/user-preferences', ); 
+
+module.exports = userPref; 
