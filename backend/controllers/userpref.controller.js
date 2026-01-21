3ccf8d3e1211897b8891e9945f518b75a45abbc2
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-20 22:52:14 -0500

    organizing and creating the file structure

--- backend/controllers/userpref.controller.js
+++ backend/controllers/userpref.controller.js
@@ -1,4 +1,8 @@
-// need to import the database here as such to push in the carApi data -> need to create a schema. 
+// need to import the database here as such to push in the carApi data -> need to create a schema.  
+
+// I can also set up the API route where this data is called with another function taking in that parameter. 
+
+// have another function for processing user that can be called in another controller. 
 
 const handleUserPref = async (req, res) => {  
     // lets set this up with a try and catch case here as such 
@@ -10,7 +14,8 @@ const handleUserPref = async (req, res) => {
                 Error: 'No response from user to fill out relevant key values'
             }); 
         } 
-        // just for some testing purposes: 
+        // just for some testing purposes:  
+        // we can implement something like the ranges etc. 
         console.log(UserCarPreference);   
         res.status(200).json({ 
             UserCarPreference: true
