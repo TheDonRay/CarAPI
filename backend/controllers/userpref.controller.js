23c7edd4e759a64f2e06cb5a50c2ff2da35f2279
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-01-21 19:20:11 -0500

    still working on this API

--- backend/controllers/userpref.controller.js
+++ backend/controllers/userpref.controller.js
@@ -1,8 +1,7 @@
-// need to import the database here as such to push in the carApi data -> need to create a schema.  
+//TODO: need to import the database here as such to push in the carApi data -> need to create a schema.  
+//TODO: need to set up openAI api as well in a seperate route that gets the helperFunction data as well.  
 
-// I can also set up the API route where this data is called with another function taking in that parameter. 
-
-// have another function for processing user that can be called in another controller. 
+const helperFunction = require('../helpers/organizedata.helper.js'); 
 
 const handleUserPref = async (req, res) => {  
     // lets set this up with a try and catch case here as such 
@@ -10,17 +9,19 @@ const handleUserPref = async (req, res) => {
         const { UserCarPreference } = req.body;  
         // do some basic checks here 
         if (!UserCarPreference) { 
-            res.status(400).json({
+            return res.status(400).json({
                 Error: 'No response from user to fill out relevant key values'
             }); 
         } 
-        console.log(UserCarPreference);   
-        res.status(200).json({ 
-            UserCarPreference: "recieved successfully"
-        });   
-        
+        console.log(UserCarPreference);    
+
         // call the function here to actually pass the organized data -> goes into the API route. 
-        
+        const OrganizedData = helperFunction(UserCarPreference);  
+
+        res.status(200).json({ 
+            UserCarPreference: "recieved successfully", 
+            message: 'recieved successfully'
+        });   
 
     } catch (error) {   
         console.error('Error with sending data to this route', error); 
---
+++ backend/helpers/organizedata.helper.js
@@ -0,0 +1,5 @@
+const organizeData = (userCarPref) => { 
+    // base case -> then call the map function to map and organize the data. 
+} 
+
+module.exports = organizeData; 
